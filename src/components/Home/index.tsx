c791bf5b84b291d31426121527b8eb3234787e8c
Alexey Tsvetkov <cai.cwet@gmail.com>
2019-12-29 18:33:31 +0300

    Add main, About, Advantage components

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,15 @@
-import "./index.sass"
+import "./index.sass";
 import React from "react";
 import { Header } from "../Header";
+import { Main } from "../Main";
+import { About } from "../About";
 
 export const Home = () => {
     return (
         <div className="Home">
             <Header/>
+            <Main/>
+            <About/>
         </div>
     )
 };
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,25 @@
+import "./index.sass"
+import React from "react";
+
+export const Main = () => {
+    return (
+        <main className="Main">
+            <div className="MainContent">
+                <div className="MainContent-Titles">
+                    <h1 className="MainContent-Title">
+                        <span>Стань</span>
+                        частью «Свезы»
+                    </h1>
+                    <h2 className="MainContent-Subtitle">
+                        Мировой лидер по производству березовой фанеры
+                    </h2>
+                </div>
+                <div className="MainContent-Buttons">
+                    <button className="MainContent-Button">Заполнить анкету</button>
+                    <button className="MainContent-Button MainContent-Button_transparent">Вакансии</button>
+                </div>
+            </div>
+            <button className="MainMore">Узнать больше</button>
+        </main>
+    )
+};
